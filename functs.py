534a2ed7911c1b9a76f25066cb40c8bdfae142fe
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-02-11 22:19:48 +0100

    merge

--- functs.py
+++ functs.py
@@ -1,6 +1,6 @@
 
 def next_avaible_drone_in(position, drones):
-    return min(drones, key=drones.ends_in_target(position))
+    return min(drones, key=lambda d: d.ends_in_target(position))
 
 
 
--- solver.py
+++ solver.py
@@ -17,35 +17,28 @@ def solve():
         if SETTINGS['n_turns'] < sums:
             break
         current_order = order_queue.pop()
-        try:
-            drone = next(dronesi)
-        except StopIteration:
-            dronesi = iter(drones)
-            drone = next(dronesi)
-        drone_weight = 0
-        drone_carry = []
+
+        drones_used_for_order = []
+        drones_used_for_order.append(next_avaible_drone_in(warehouses[0], drones))
         for product, count in current_order.products():
-            while count > 0:
-                load_amount = min([count, (SETTINGS['max_payload'] - drone_weight) // product.weight])
-                drone.commandqueue.append(Load(
-                    warehouses[0],
-                    product,
-                    load_amount
-                ))
-                drone_carry.append((product, load_amount))
-                drone_weight += load_amount * product.weight
-                warehouses[0].stock[product] -= load_amount
-                if load_amount == count:
-                    for p, c in drone_carry:
-                        drone.commandqueue.append(Deliver(current_order, p, c))
-                    try:
-                        drone = next(dronesi)
-                    except StopIteration:
-                        dronesi = iter(drones)
-                        drone = next(dronesi)
-                    drone_weight = 0
-                    drone_carry = []
+            for drone in drones_used_for_order:
+                load_amount = min([count, (SETTINGS['max_payload'] - drone.load_weight()) // product.weight])
+                drone.add_load(product.code, load_amount)
+                warehouses[0].stock[product.code] -= load_amount
                 count -= load_amount
+                if load_amount == 0:
+                    break
+            else:
+                while count > 0:
+                    drone = next_avaible_drone_in(warehouses[0], drones)
+                    drones_used_for_order.append(drone)
+                    load_amount = min([count, (SETTINGS['max_payload'] - drone_weight) // product.weight])
+                    drone.add_load(product.code, load_amount)
+                    warehouses[0].stock[product.code] -= load_amount
+                    count -= load_amount
+        for drone in drones_used_for_order:
+            for p, c in drone.load.items():
+                drone.deliver(current_order, p, c)
 
 if __name__ == "__main__":
     parse()
