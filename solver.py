3f94f33cca064928748da81b17b9c590183c531a
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-02-11 21:32:48 +0100

    testingasdfasr

--- solver.py
+++ solver.py
@@ -10,31 +10,35 @@ def solve():
 
     dronesi = iter(drones)
 
-    current_order = order_queue.pop()
-    drone = next(dronesi)
-    drone_weight = 0
-    drone_carry = []
-    for product, count in current_order.products():
-        while count > 0:
-            load_amount = min([count, (SETTINGS['max_payload'] - drone_weight) // product.weight])
-            drone.commandqueue.append(Load(
-                warehouses[0],
-                product,
-                load_amount
-            ))
-            drone_carry.append((product, load_amount))
-            drone_weight += load_amount * product.weight
-            if load_amount == count:
-                for p, c in drone_carry:
-                    drone.commandqueue.append(Deliver(current_order, p, c))
-                drone = next(dronesi)
-                drone_weight = 0
-                drone_carry = []
-            count -= load_amount
+    while True:
+        current_order = order_queue.pop()
+        drone = next(dronesi)
+        drone_weight = 0
+        drone_carry = []
+        for product, count in current_order.products():
+            while count > 0:
+                load_amount = min([count, (SETTINGS['max_payload'] - drone_weight) // product.weight])
+                drone.commandqueue.append(Load(
+                    warehouses[0],
+                    product,
+                    load_amount
+                ))
+                drone_carry.append((product, load_amount))
+                drone_weight += load_amount * product.weight
+                if load_amount == count:
+                    for p, c in drone_carry:
+                        drone.commandqueue.append(Deliver(current_order, p, c))
+                    drone = next(dronesi)
+                    drone_weight = 0
+                    drone_carry = []
+                count -= load_amount
 
 if __name__ == "__main__":
     parse()
-    solve()
+    try:
+        solve()
+    except StopIteration:
+        pass
     print(sum(len(d.commandqueue) for d in SETTINGS['drones']))
     for d in SETTINGS['drones']:
         for c in d.commandqueue:
